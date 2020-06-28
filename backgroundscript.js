80fa1c39c369d93d406d4f9239419ced05cdc002
Kanish Aggarwal <43958616+kanish51@users.noreply.github.com>
2020-06-29 04:02:25 +0530

    Add files via upload

---
+++ backgroundscript.js
@@ -0,0 +1,32 @@
+chrome.contextMenus.create({
+	'title': 'Search IMDB for "%s"',
+	'contexts': ['selection'],
+	'onclick': async (context) => {
+		const name = context.selectionText;
+		const response = await fetch(`https://www.omdbapi.com/?t=${name}&apikey=YOUR_API_KEY`)
+		const {
+			Title,
+			Year,
+			Runtime,
+			Genre,
+			Actors,
+			Plot,
+			imdbRating
+		} = await response.json()
+		const newLine = "\r\n"
+		let message = `Title : ${Title}`
+		message += newLine
+		message += `Year : ${Year}`
+		message += newLine
+		message += `IMDb Rating : ${imdbRating}`
+		message += newLine
+		message += `Runtime : ${Runtime}`
+		message += newLine
+		message += `Genre : ${Genre}`
+		message += newLine
+		message += `Actors : ${Actors}`
+		message += newLine
+		message += `Plot : ${Plot}`
+		alert(message)
+	}
+});
